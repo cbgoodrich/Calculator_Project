15c188f98bcef3a88b6a4025fbda2b097c95e750
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-11-01 13:51:43 -0400

    Updated from Brython Server: 11/1/2017 1:51:42 PM

--- calculatorApplet.py
+++ calculatorApplet.py
@@ -1,1 +1,36 @@
+#Charlie Goodrich
+#11/01/17
+#calculatorApplet.py - displays a 4-function calculator that executes commands
 
+from ggame import *
+
+#CONSTANTS
+CALC_X = 300
+CALC_Y = 500
+
+
+def processNumber(num):
+    
+def compute():
+    
+def clear():
+    
+def mouseClick(event):
+    
+
+
+if __name__ == "__main__":
+    
+    red = Color(0xFF0000, 1)
+    white = Color(0x000000, 0)
+    black = Color(0x000000, 1)
+    
+    background = RectangleAsset(CALC_X, CALC_Y, LineStyle(1, black), white)
+    
+    Sprite(background)
+    
+    App().run()
+    
+    
+    
+    
