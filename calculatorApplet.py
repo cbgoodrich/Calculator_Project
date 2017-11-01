fb3fbb1eaed940041a3299dbcc57c6cefaa8eb29
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-11-01 13:59:45 -0400

    Updated from Brython Server: 11/1/2017 1:59:44 PM

--- calculatorApplet.py
+++ calculatorApplet.py
@@ -5,17 +5,26 @@
 from ggame import *
 
 #CONSTANTS
-CALC_X = 300
+WINDOW_X = 1000
+WINDOW_Y = 530
+CALC_X = 400
 CALC_Y = 500
+BUTTON_X = 60
+BUTTON_Y = 60
+ANSWER_X = 350
 
 
 def processNumber(num):
+    var = 0
     
 def compute():
+    var = 0
     
 def clear():
+    var = 0
     
 def mouseClick(event):
+    var = 0
     
 
 
@@ -26,9 +35,10 @@ if __name__ == "__main__":
     black = Color(0x000000, 1)
     
     background = RectangleAsset(CALC_X, CALC_Y, LineStyle(1, black), white)
+    answerBox = RectangleAsset(ANSWER_X, BUTTON_Y, LineStyle(1, black), white)
     
-    Sprite(background)
-    
+    Sprite(background, (WINDOW_X/2-CALC_X/2, WINDOW_Y/2-CALC_Y/2))
+    Sprite(answerBox, (WINDOW_X/2-ANSWER_X/2, BUTTON_Y/2))
     App().run()
     
     
