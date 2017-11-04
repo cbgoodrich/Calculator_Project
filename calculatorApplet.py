54a28d8b51a4a40f3025ffbf93741c951386de0b
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-11-03 21:38:04 -0400

    Updated from Brython Server: 11/3/2017 9:38:03 PM

--- calculatorApplet.py
+++ calculatorApplet.py
@@ -64,6 +64,8 @@ def compute():
         print(float(data["number1"])/float(data["number2"]))
     elif data["operations"] == "*":
         print(float(data["number1"])*float(data["number2"]))
+    elif data["operations"] == "-":
+        print(float(data["number1"])-float(data["number2"]))
     
 
 if __name__ == "__main__":
