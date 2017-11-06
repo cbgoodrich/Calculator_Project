d18747757f45f4df64752400e3f67ceaf378c10f
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-11-06 14:54:34 -0500

    Updated from Brython Server: 11/6/2017 2:54:32 PM

--- calculatorApplet.py
+++ calculatorApplet.py
@@ -17,11 +17,15 @@ DIFF = 20
 #NUMBER PROCESSING FUNCTION
 def processNumber(num):
     if data["operations"] == "":
+        data["num1Text"].destroy()
         data["number1"] += str(num)
         data["num1"] = TextAsset(data["number1"], fill = black, style = "Bold 24pt Times")
         data["num1Text"] = Sprite(data["num1"], (ansBox.x + 10, ansBox.y + BUTTON_Y/2 - 15))
     else:
-        data["num1Text"].destroy()
+        while data["i"] == 1:
+            data["num1Text"].destroy()
+            data["i"] += 1
+        data["num2Text"].destroy()
         data["number2"] += str(num)
         data["num2"] = TextAsset(data["number2"], fill = black, style = "Bold 24pt Times")
         data["num2Text"] = Sprite(data["num2"], (ansBox.x + 10, ansBox.y + BUTTON_Y/2 - 15))
@@ -73,6 +77,7 @@ def clear():
         data["number2"] = ""
     if data["operations"] != "":
         data["operations"] = ""
+    data["ans"].destroy()
 
 #COMPUTING FUNCTION
 def compute():
@@ -80,20 +85,21 @@ def compute():
     if data["operations"] == "/":
         data["answer/"] = float(data["number1"])/float(data["number2"])
         data["answer"] = TextAsset(data["answer/"], fill = black, style = "Bold 24pt Times")
-        Sprite(data["answer"], (ansBox.x + 10, ansBox.y + BUTTON_Y/2 - 15))
+        data["ans"] = Sprite(data["answer"], (ansBox.x + 10, ansBox.y + BUTTON_Y/2 - 15))
     elif data["operations"] == "*":
         data["answer*"] = float(data["number1"])*float(data["number2"])
         data["answer"] = TextAsset(data["answer*"], fill = black, style = "Bold 24pt Times")
-        Sprite(data["answer"], (ansBox.x + 10, ansBox.y + BUTTON_Y/2 - 15))
+        data["ans"] = Sprite(data["answer"], (ansBox.x + 10, ansBox.y + BUTTON_Y/2 - 15))
     elif data["operations"] == "-":
         data["answer-"] = float(data["number1"])-float(data["number2"])
         data["answer"] = TextAsset(data["answer-"], fill = black, style = "Bold 24pt Times")
-        Sprite(data["answer"], (ansBox.x + 10, ansBox.y + BUTTON_Y/2 - 15))
+        data["ans"] = Sprite(data["answer"], (ansBox.x + 10, ansBox.y + BUTTON_Y/2 - 15))
     elif data["operations"] == "+":
         data["answer+"] = float(data["number1"])+float(data["number2"])
         data["answer"] = TextAsset(data["answer+"], fill = black, style = "Bold 24pt Times")
-        Sprite(data["answer"], (ansBox.x + 10, ansBox.y + BUTTON_Y/2 - 15))
-    
+        data["ans"] = Sprite(data["answer"], (ansBox.x + 10, ansBox.y + BUTTON_Y/2 - 15))
+    data["number1"] = ""
+    data["number2"] = ""
 
 if __name__ == "__main__":
     
@@ -102,11 +108,14 @@ if __name__ == "__main__":
     data["number1"] = ""
     data["number2"] = ""
     data["operations"] = ""
+    data["i"] = 1
     
     #COLORS
     red = Color(0xFF0000, 1)
     white = Color(0x000000, 0)
     black = Color(0x000000, 1)
+    data["num1Text"] = Sprite(TextAsset("", fill = black, style = "Bold 24pt Times"))
+    data["num2Text"] = Sprite(TextAsset("", fill = black, style = "Bold 24pt Times"))
     
     #CREATING THE BASIS FOR THE CALCULATOR
     background = RectangleAsset(CALC_X, CALC_Y, LineStyle(1, black), white)
